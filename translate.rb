168790afa90a9ffc3975c54ae667e7795e7176c1
Dan Madere <dan@danmadere.com>
2025-09-22 20:10:54 -0400

    auto-yes in ffmpeg cmd

--- translate.rb
+++ translate.rb
@@ -25,7 +25,7 @@ require 'fileutils'
 # extract the first subtitle track from an MKV video
 def extract_subtitles(video_path, output_srt, stream)
   puts "extracting subtitles from #{video_path} to #{output_srt.path}"
-  command = "ffmpeg -i \"#{video_path}\" -map #{stream} \"#{output_srt.path}\""
+  command = "ffmpeg -y -i \"#{video_path}\" -map #{stream} \"#{output_srt.path}\""
   puts "Command: #{command}"
   if system(command)
     puts "Subtitles extracted successfully to #{output_srt.path}"
